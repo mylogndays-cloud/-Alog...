a9ebf8f3f8550f41b4330ee9b4451bb807ef2a5b
mylogndays-cloud <mylogndays@gmail.com>
2026-04-21 16:31:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ function checkID() {
     if (inputVal.length > 0) {
         inputPage.style.display = "none";
         resultPage.style.display = "block";
-        displayID.innerText = "Your Memory ID:" + inputVal;
+        displayID.innerText = "ID: " + inputVal;
         
-        // 嘗試播放對應 ID 的錄音檔
-        player.src = "RECORDS/" + inputVal + ".wav";
+        // --- 修正後的路徑設定 ---
+        const bucketName = "memory-log-de585.firebasestorage.app"; 
+        // 注意：路徑前綴 RECORDS/ 必須轉換為 RECORDS%2F
+        const filePath = "RECORDS%2F" + inputVal + ".wav";
+        
+        // 使用正確的 Firebase Storage 公開網址格式
+        const firebaseUrl = `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${filePath}?alt=media`;
+        
+        player.src = firebaseUrl;
         player.load();
+        
+        player.onerror = function() {
+            console.error("Searching Your Memory...");
+            // 友善提示：可以在畫面顯示「記憶密封中，請稍候再試」
+        };
     } else {
         alert("Please enter a valid ID!");
     }
